ad54865ff769eda4dbc2315f858df7c0c4651e06
Oswaldo Cavalcante <jocmail@gmail.com>
2023-06-26 09:08:35 -0300

    Setting version for bug fix

--- posttobanner.php
+++ posttobanner.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Post to Banner
  * Plugin URI:        https://github.com/oswaldocavalcante/posttobanner
  * Description:       Create a image banners from WordPress posts to share on social media.
- * Version:           3.1.1
+ * Version:           3.1.2
  * Author:            Oswaldo Cavalcante
  * Author URI:        https://oswaldocavalcante.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'POSTTOBANNER_VERSION', '3.1.1' );
+define( 'POSTTOBANNER_VERSION', '3.1.2' );
 
 // Include Updater file
 if( ! class_exists( 'Posttobanner_Updater' ) ){
